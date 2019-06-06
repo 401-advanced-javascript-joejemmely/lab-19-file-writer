933cbc5c13094ba9bed7202966bf6e010d8194aa
Joé Jemmely <joe.jemmely@gmail.com>
2019-06-06 16:21:48 -0700

    Update

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
 'use strict';
 
 const fs = require('fs');
+const { promisify } = require('util');
 
-const alterFile = (file) => {
-  fs.readFile( file, (err, data) => {
-    if(err) { throw err; }
-    let text = data.toString().toUpperCase();
-    fs.writeFile( file, Buffer.from(text), (err, data) => {
-      if(err) { throw err; }
-      console.log(`${file} saved`);
-    });
-  });
+const readFileAsync = promisify(fs.readFile);
+const writeFileAsync = promisify(fs.writeFile);
+
+const events = require('./utils/events.js');
+const QClient = require('@nmq/q/client');
+
+/**
+ * Async function to read, transform, and write
+ * @param {*} file
+ */
+const alterFile = async file => {
+  try {
+    const buffer = await readFileAsync(file);
+    const upperCasedContent = buffer.toString().toUpperCase();
+    await writeFileAsync(file, Buffer.from(upperCasedContent));
+    await QClient.publish('files', events.files.SAVE, upperCasedContent);
+  } catch (error) {
+    await QClient.publish(
+      'files',
+      events.files.ERROR,
+      'Something went horribly wrong…'
+    );
+  }
 };
 
 let file = process.argv.slice(2).shift();
